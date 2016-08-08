3d30b6179bbe79591429484a2aa8babad95531fb
Jake Boyles <jake@jibdesigns.com>
2016-08-08 14:51:22 -0400

    allow useres to give locations etc

--- src/App.js
+++ src/App.js
@@ -8,24 +8,45 @@ var Main = React.createClass({
   getInitialState: function(){
     return {
       data:[],
+      value: "Cincinnati"
     }
   },
 
   componentWillMount: function() {
-    this.serverRequest = $.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${this.props.location}&mode=json&units=imperial&cnt=7&APPID=7cf16558d759d14815306832bd7341e2`, (result) => {
+    this.serverRequest = $.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${this.state.value}&mode=json&units=imperial&cnt=7&APPID=7cf16558d759d14815306832bd7341e2`, (result) => {
       this.setState({
         data: result.list,
       });
     });
   },
 
+handleCityChange: function(e) {
+   this.setState({value: e.target.value});
+},
+
+handleChange: function(event) {
+    this.setState({value: this.state.value});
+
+    this.serverRequest = $.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${this.state.value}&mode=json&units=imperial&cnt=7&APPID=7cf16558d759d14815306832bd7341e2`, (result) => {
+      this.setState({
+        data: result.list,
+      });
+    });
+
+    event.preventDefault();
+  },
+
   render: function(){
-    console.log("App:",this.state.data);
 
       return (
           <div>
+          <form onSubmit={this.handleChange}>
+            <input onChange={this.handleCityChange} type="text" />
+            <button type="submit">Go</button>
+          </form>
+
           {this.state.data.map(w =>
-            <Day key={w} data={w} />
+            <Day key={w.temp.day} data={w} />
           )}
           </div>
       ); 
--- src/Day.js
+++ src/Day.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import moment from 'moment';
 
 const Day = ({ data }) => {
+  let time = moment.unix(data.dt).format("dddd");
   return (
       <div>
-          <h2 key={data.temp.day}>The temp will be: {data.temp.day}</h2>
+          <h2>The temp will be: {data.temp.day} on {time} </h2>
       </div>
   );
 };
