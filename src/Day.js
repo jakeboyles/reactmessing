25a17cf1cb1c024231ffb45afec987606aee85c6
Jake Boyles <jake@jibdesigns.com>
2016-08-14 16:11:57 -0400

    adding in icons and stuff

--- src/Day.js
+++ src/Day.js
@@ -3,12 +3,18 @@ import moment from 'moment';
 import './App.css';
 
 const Day = function Day({ data }) {
+  let propTypes = {
+    data: React.PropTypes.object,
+  };
   let time = moment.unix(data.dt).format('dddd');
+  let icon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
   return (
     <div className="day">
       <h4>{time}</h4>
       <h2>Max: {data.temp.max}</h2>
       <h2>Min: {data.temp.min}</h2>
+      <p className="paragraph">{data.weather[0].description}</p>
+      <img className="icon" alt="weather icon" src={icon} />
     </div>
   );
 };
